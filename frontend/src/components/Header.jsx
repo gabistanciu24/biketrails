b99c362718d7c64e8b06e404cf2c5c4363f7bf82
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-04-27 17:39:38 +0300

    Mobile responsive navbar.

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -19,9 +19,22 @@ const Header = () => {
           <img src={images.Logo} alt="logo" className={styles.logo} />
         </div>
         <div className={styles.burger}>
-          <TiThMenu className={styles.burger_icon} />
+          {navIsVisible ? (
+            <IoClose
+              className={styles.burger_icon}
+              onClick={navVisibilityHandler}
+            />
+          ) : (
+            <TiThMenu
+              className={styles.burger_icon}
+              onClick={navVisibilityHandler}
+            />
+          )}
         </div>
-        <div className={styles.navbar}>
+        <div
+          className={styles.navbar}
+          style={{ top: navIsVisible ? "0" : "-70rem" }}
+        >
           <ul className={styles.navbar_items}>
             <li>
               <a href="/">Acasă</a>
