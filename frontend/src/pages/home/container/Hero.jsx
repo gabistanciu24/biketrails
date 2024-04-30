d5ef9f989d404507a37b544446be95421e223686
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-04-30 22:19:34 +0300

    Add fade-in fade-out transition when hovering navbar buttons.

--- frontend/src/pages/home/container/Hero.jsx
+++ frontend/src/pages/home/container/Hero.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import styles from "./hero_styles/hero.module.css";
+import { images } from "../../../constants";
 
 const Hero = () => {
   return (
     <section className={styles.hero} id="acasa">
       <div className={styles.container}>
         <div className={styles.content}>
+          <img src={images.Logo} alt="logo" className={styles.logo} />
           <span className={styles.subtitle}>Bike - Trails</span>
           <h1 className={styles.title}>Aventură pe două roți</h1>
           <p className={styles.description}>
