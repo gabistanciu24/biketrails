4b0f24df9106438a96848c8efa8fe1ac6d323448
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-05-05 21:53:22 +0300

    Add button for trails page.

--- frontend/src/pages/home/container/Trails.jsx
+++ frontend/src/pages/home/container/Trails.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import TrailCard from "./TrailCard";
 import styles from "./styles/trails.module.css";
+import { FaArrowRight } from "react-icons/fa";
 
 const Trails = () => {
   return (
@@ -9,6 +10,10 @@ const Trails = () => {
       <TrailCard className={styles.trail_card} />
       <TrailCard className={styles.trail_card} />
       <TrailCard className={styles.trail_card} />
+      <button className={styles.button}>
+        <span>Mai multe trasee </span>
+        <FaArrowRight className={styles.arrow} />
+      </button>
     </section>
   );
 };
