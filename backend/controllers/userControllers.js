d4ec2ab01b91acec0e526f4abd1201c75d4996fe
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-05-19 15:27:22 +0300

    Custom error handler.

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,13 +1,14 @@
 import User from "../models/User.js";
 
-export const registerUser = async (req, res) => {
+export const registerUser = async (req, res, next) => {
   try {
     const { name, email, password } = req.body;
     //check if user exists or not
     let user = await User.findOne({ email });
 
     if (user) {
-      return res.status(400).json({ message: "User have already registered" });
+      //return res.status(400).json({ message: "User have already registered" });
+      throw new Error("User have already registered");
     }
 
     //creating a new user
@@ -27,6 +28,7 @@ export const registerUser = async (req, res) => {
       token: await user.generateJWT(),
     });
   } catch (error) {
-    return res.status(500).json({ message: "Something went wrong!" });
+    //return res.status(500).json({ message: "Something went wrong!" });
+    next(error);
   }
 };
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,10 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import {
+  errorResponserHandler,
+  invalidPathHandler,
+} from "./middleware/errorHandler.js";
 
 import userRoutes from "./routes/userRoutes.js";
 
@@ -14,6 +18,10 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/users", userRoutes);
+
+app.use(invalidPathHandler);
+
+app.use(errorResponserHandler);
 
 const PORT = process.env.PORT || 5000;
 
