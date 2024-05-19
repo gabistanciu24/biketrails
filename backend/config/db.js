73f901039d5df5523456fefe24ebe2c268ccff0e
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-05-19 14:29:43 +0300

    User registration API.

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,9 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
 
-const url = "mongodb://127.0.0.1:27017/bike_trails";
+dotenv.config();
+
+const url = process.env.url;
 
 const connectDB = async () => {
   try {
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 
+import userRoutes from "./routes/userRoutes.js";
+
 dotenv.config();
 connectDB();
 const app = express();
@@ -10,6 +12,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Server is running...");
 });
+
+app.use("/api/users", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
