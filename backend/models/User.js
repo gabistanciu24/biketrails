9534ac5f1d042688e461dc7ef434e7d1cb45f3da
Stanciu Gabriel <gabistanciu24@yahoo.com>
2024-05-20 11:12:34 +0300

    Remove verified from user schema.

--- backend/models/User.js
+++ backend/models/User.js
@@ -12,7 +12,6 @@ const UserSchema = new Schema(
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    verified: { type: Boolean, default: false },
     verificationCode: { type: String, required: false },
     admin: { type: Boolean, default: false },
   },
